44b3b401342faea9f985d5523bf0ec93e83d2757
zhanghuan <huan.zhang@bondex.com.cn>
2022-08-15 11:28:53 +0800

    init: 初始化项目;

---
+++ src/main/java/site/xiaobu/bmschedule/BmScheduleApplication.java
@@ -0,0 +1,13 @@
+package site.xiaobu.bmschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmScheduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/xiaobu/bmschedule/BmScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package site.xiaobu.bmschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
